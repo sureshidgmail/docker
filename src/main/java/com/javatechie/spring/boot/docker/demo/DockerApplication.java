f6fd042d51d1e5289786ef741fd50d54215f69d1
sureshidgmail <sureshid@gmail.com>
2025-12-28 11:06:06 +0530

    first commit

---
+++ src/main/java/com/javatechie/spring/boot/docker/demo/DockerApplication.java
@@ -0,0 +1,22 @@
+package com.javatechie.spring.boot.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerApplication {
+
+    @GetMapping("/message")
+    public String getMessage()
+    {
+        return "Hello World! This is my first Dockerized Spring Boot Application";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+
+}
